ccb59391c20dc60e817384ee8b7c4787f630dee1
Shivani07101996 <shivani@policyadvisor.com>
2025-03-18 11:25:36 -0400

    enabled copy functionality

--- app/components/commandActivation.tsx
+++ app/components/commandActivation.tsx
@@ -24,6 +24,7 @@ export default function CommandActivation() {
     const [rating,setRating] = useState(null);
     const [AIResponse,setAIResponse] = useState(false)
     const [pendingElaboration,setPendingElaboration]=useState<{message_id: string, event: any} | null>(null)
+    const [contentCopy,setContentCopy]=useState<boolean>(false);
 
 
     // START OF 2022 Code *********************************************************************************************
@@ -630,6 +631,16 @@ export default function CommandActivation() {
         setDetailMode((prev)=>isChecked);
     }
 
+    const copyContent = async(event:any,message:string) => {
+        try {
+            await navigator.clipboard.writeText(message);
+            setContentCopy(true)
+            setTimeout(() => setContentCopy(false), 2000);
+        } catch (err) {
+            console.error("Failed to copy: ", err);
+        }
+    }
+
     // bg-[rgb(222,233,235)]
     return (
             <div className='flex h-screen text-sm' style={{ fontFamily: 'Lato, sans-serif' }}>
@@ -776,11 +787,16 @@ export default function CommandActivation() {
                                                                     {message.rating === 0 ? <img src="/thumbsDownColored.svg" className="w-4 " /> : <img src="/thumbsDownNonColored.svg" className="w-4" />}
                                                                 </button>
 
-                                                                <button className={`px-2 mr-2 hover:scale-105 hover:shadow bg-gray-100 cursor-pointer rounded-md h-6 text-sm flex items-center gap-1`} data-id={`copy_${message.messageId}`} >
+                                                                <button className={`px-2 mr-2 hover:scale-105 hover:shadow bg-gray-100 cursor-pointer rounded-md h-6 text-sm flex items-center gap-1`} data-id={`copy_${message.messageId}`} onClick={(e)=>copyContent(e,messageWithoutSources)} >
+                                                                    {!contentCopy ?
                                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" 
                                                                     strokeWidth="1.5" stroke="currentColor" className="w-4 text-gray-400">
                                                                         <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5A3.375 3.375 0 0 0 6.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0 0 15 2.25h-1.5a2.251 2.251 0 0 0-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 0 0-9-9Z" />
+                                                                    </svg> : 
+                                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 text-gray-400">
+                                                                        <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
                                                                     </svg>
+                                                                    }
                                                                 </button>
                                                             </div>
                                                             <div className='mt-2'>
