ccf1d10190f56c24d5861a7e8c74deae7e96ba89
Shivani07101996 <shivani@policyadvisor.com>
2025-04-23 11:36:01 -0400

    created audio ai

---
+++ app/custom-jsx.d.ts
@@ -0,0 +1,12 @@
+// custom-jsx.d.ts
+import 'react'
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'elevenlabs-convai': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+        'agent-id': string
+      }
+    }
+  }
+}
